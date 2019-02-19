3c9b55206f2d9d2e2f93e5648798a0fbbff1516d
salyateem13 <31292258+salyateem13@users.noreply.github.com>
2019-02-19 10:08:59 -0500

    added carousel module

--- js/responsive-nav.js
+++ js/responsive-nav.js
@@ -10,6 +10,18 @@ $(document).ready(function() {              // attach a handler to an event for 
        $(".open-submenu").toggleClass("open-submenu-activated");
        $(".dropdown").slideToggle();
     });
+
+
+    $('.carousel').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        arrows: false,
+        mobileFirst: true
+        
+      });
+
    
 });
 
@@ -19,9 +31,57 @@ $(window).scroll(function(){
   
     if (scroll >= 1) sticky.addClass('dark-background');
     else sticky.removeClass('dark-background');
+
+
+
+
   });
-  
-/*
+
+  /*
+
+
+var slideIndex = 1;
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = $(".carousel-item");
+  var dots = $(".dot");
+  if (n > slides.length) {slideIndex = 1} 
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block"; 
+  dots[slideIndex-1].className += " active";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
  //initial class selector for mobile or desktop nav
     $(window).bind("load", function () {
